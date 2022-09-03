218375060dd0fd3cc679727523d7ec29d9e4c31a
Nakib <Nakkibcse@gmail.com>
2022-09-03 22:34:11 +0600

    load category list

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// loading category list
+const loadCategory = () => {
+    const url = "https://openapi.programming-hero.com/api/news/categories";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => showNewsCategory(data.data.news_category))
+      .catch((error) => console.log(error));
+  };
